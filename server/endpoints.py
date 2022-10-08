0f5a088c6b00b8669ad60c44ad414c78ff515558
Kang In Park <kip218@nyu.edu>
2022-10-08 16:15:44 -0400

    setting up home page as default landing page & set api doc path to /api/doc using flask blueprint and api endpoint as /api/ - see https://flask-restx.readthedocs.io/en/latest/swagger.html#customization for documentation

--- server/endpoints.py
+++ server/endpoints.py
@@ -3,12 +3,14 @@ This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
 
-from flask import Flask, render_template
+from flask import Flask, Blueprint, render_template
 from flask_restx import Resource, Api
 # import db.db as db
 
 app = Flask(__name__)
-api = Api(app)
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint, doc='/doc/')
+app.register_blueprint(blueprint)
 
 LIST = 'list'
 HELLO = '/hello'
@@ -63,6 +65,6 @@ class Endpoints(Resource):
         return {"Available endpoints": endpoints}
 
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
