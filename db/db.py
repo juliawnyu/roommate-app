247cdaa13cfa8089c803cda1f1abcfbe4486f8a6
Kang In Park <kip218@nyu.edu>
2023-02-23 18:24:24 -0500

    added remove_user() and deleted sqlite3 code

--- db/db.py
+++ db/db.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -40,6 +38,9 @@ class DB_Manager:
                     'grade': grade}
         self.users.insert_one(new_user)
 
+    def remove_user(self, netID):
+        self.users.delete_one({'netID': netID})
+
     def login_correct(self, netID, password):
         user = self.get_user(netID)
         if bcrypt.checkpw(password.encode(), user['password']):
@@ -72,72 +73,3 @@ class DB_Manager:
 
 #     print("Getting user...")
 #     print(db.get_user('kip218'))
-
-
-db_file = 'database.db'
-
-
-# This is a temporary DB class for SQLite3
-class DB_Users:
-    def __init__(self):
-        self.con = sqlite3.connect(db_file, check_same_thread=False)
-        self.cur = self.con.cursor()
-        self.reset_table()
-
-    # Create new users table - reset table if it already exists
-    # Only use for setting up db environment locally for testing
-    def reset_table(self):
-        self.cur.execute("DROP TABLE IF EXISTS users")
-        self.cur.execute("CREATE TABLE users(netID, password, grade)")
-
-    # Fetch all from users table
-    def fetch_all(self):
-        res = self.cur.execute("SELECT * FROM users")
-        return res.fetchall()
-
-    # Add new user into users table
-    # Todo - store password hash instead of plaintext
-    # Todo - check if user account already exists
-    def add_new_user(self, netID, password, grade):
-        args = (netID, password, grade)
-        try:
-            self.cur.execute("INSERT INTO users VALUES (?, ?, ?)", args)
-            self.con.commit()
-        except Exception as error:
-            print(f"Error in add_new_user(): {error}")
-            return False
-        return True
-
-    # Retrieve user info for login
-    def check_login(self, netID, password):
-        args = (netID, password)
-        try:
-            res = self.cur.execute(
-                "SELECT * FROM users WHERE netID=? AND password=?", args)
-            res = res.fetchall()
-        except Exception as error:
-            print(f"Error in check_login(): {error}")
-            return False
-
-        # only return True if login attempt retrieves a single user account
-        if len(res) == 1:
-            return True
-        else:
-            return False
-
-    # Check if user already exists
-    def user_exists(self, netID):
-        args = (netID)
-        try:
-            res = self.cur.execute(
-                "SELECT * FROM users WHERE netID=?", args)
-            res = res.fetchall()
-        except Exception as error:
-            print(f"Error in check_login(): {error}")
-            return False
-
-        # return true if user netID exists in database
-        if len(res) >= 1:
-            return True
-        else:
-            return False
