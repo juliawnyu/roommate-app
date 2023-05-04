dd5f73bd5738579a4cee3decd100f5a88f9806f3
Kang In Park <kip218@nyu.edu>
2023-05-04 01:00:50 -0400

    added tests for db.py

--- db/tests/test_db.py
+++ db/tests/test_db.py
@@ -2,6 +2,8 @@ import db.db as dbc
 
 import pytest
 
+dbc.MONGO_ENV = 'LOCAL'
+
 TEST_DB = dbc.USERS
 TEST_NETID = 'bm2815'
 TEST_PASSWORD = 'password'
@@ -15,3 +17,29 @@ def temp_rec():
         db.add_user(TEST_NETID, TEST_PASSWORD, TEST_GRADE)
         yield
         db.remove_user(TEST_NETID)
+
+
+def test_reset_db(temp_rec):
+    db = dbc.DB_Manager()
+    db.reset_db()
+    assert not db.get_all()
+
+
+def test_get_user(temp_rec):
+    db = dbc.DB_Manager()
+    assert db.get_user(TEST_NETID)
+
+
+def test_get_all(temp_rec):
+    db = dbc.DB_Manager()
+    assert db.get_all()
+
+
+def test_login_correct(temp_rec):
+    db = dbc.DB_Manager()
+    assert db.login_correct(TEST_NETID, TEST_PASSWORD)
+
+
+def test_login_incorrect(temp_rec):
+    db = dbc.DB_Manager()
+    assert not db.login_correct(TEST_NETID, "WRONG PASSWORD")
